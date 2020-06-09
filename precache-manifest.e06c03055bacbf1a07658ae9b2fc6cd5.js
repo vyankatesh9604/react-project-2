f873564605265691bcf325ee78def113ce5fa184
vyankatesh9604 <vyankateshgadpallewar12@gmail.com>
2020-06-09 14:00:21 +0530

    Updates

---
+++ precache-manifest.e06c03055bacbf1a07658ae9b2fc6cd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "651f6f5e5d9fb3abb43828329b8f6b90",
+    "url": "/react-project-2/index.html"
+  },
+  {
+    "revision": "ecb8f9724b8aba2e9913",
+    "url": "/react-project-2/static/css/main.c84df392.chunk.css"
+  },
+  {
+    "revision": "ea208b4e30ddeff10793",
+    "url": "/react-project-2/static/js/2.a696bc23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-project-2/static/js/2.a696bc23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecb8f9724b8aba2e9913",
+    "url": "/react-project-2/static/js/main.a7af01c3.chunk.js"
+  },
+  {
+    "revision": "13ed4c753570fb7f3923",
+    "url": "/react-project-2/static/js/runtime-main.fcfc15b1.js"
+  }
+]);
